0cc9e62b44fdb34df1f1e2d7015eba95f9ce9b0a
Kanishka <kanishkamahale18@gmail.com>
2026-01-26 01:35:14 +0530

    enemy script and health bar half

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -3,14 +3,18 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-
+var health=50
+var health_max=50
+var health_min=0
 @onready var timer: Timer = $Timer
-
-
-func _physics_process(delta: float) -> void:
+@onready var player=get_node("/root/game/player")
+func _physics_process(_delta: float) -> void:
+	var direction_x=sign(player.global_position.x-global_position.x)
+	velocity.x=direction_x*100.0
 	# Add the gravity.
 	move_and_slide()
 
+	
 
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("Player2"):
--- player_2.gd
+++ player_2.gd
@@ -1,9 +1,11 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 200.0
 const JUMP_VELOCITY = -400.0
-
+var health=100
+var health_max=100
+var health_min=0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
